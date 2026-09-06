888a507c8c7558eb67f3cf0d0dfe93e91800d49f
lotgomez <lotjgomez@gmail.com>
2026-09-06 02:08:25 -0300

    mapa

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 8.0.5/22607
 // Filename: feather_orb.ggsk
-// Generated 2026-09-06T05:02:01Z
+// Generated 2026-09-06T05:04:11Z
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_sounds_splashscreen', 2, false, { ignoreInState: 1  });
